a58cbe8ec39514feab554b2f17643bb369ddd218
shafoharuki <muhammadsyafizuraimi@gmail.com>
2020-06-17 21:42:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import FlashcardList from "./FlashcardList";
+
+function App() {
+  //eslint-disable-next-line
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  return (
+    <div className="App">
+      <FlashcardList flashcards={flashcards} />
+    </div>
+  );
+}
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: "What is 2 + 2 ?",
+    answer: "4",
+    options: ["2", "3", "4", "5"],
+  },
+  {
+    id: 2,
+    question: "What is 24 + 32 ?",
+    answer: "56",
+    options: ["62", "43", "47", "56"],
+  },
+  {
+    id: 3,
+    question: "What is 33 + 99 ?",
+    answer: "132",
+    options: ["113", "132", "143", "122"],
+  },
+];
+
+export default App;
